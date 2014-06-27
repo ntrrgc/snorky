d4f6c158a0a2137c48b54e2da92fdcd3a343f2b7
Juan Luis Boya García <ntrrgc@gmail.com>
2014-06-27 04:28:36 +0200

    Do not rely on setTimeout() for promise testing

--- snorky/fronted/src/spec/promises.spec.js
+++ snorky/fronted/src/spec/promises.spec.js
@@ -6,24 +6,15 @@ describe("Promises", function() {
       self.success = success;
       self.error = error;
     });
-
-    self.thenCallback = jasmine.createSpy("thenCallback");
-    self.errorCallback = jasmine.createSpy("errorCallback");
   });
 
   it("call their callbacks", function(done) {
     var self = this;
 
-    self.promise.then(self.thenCallback, self.errorCallback);
-    expect(self.thenCallback).not.toHaveBeenCalled();
-    expect(self.errorCallback).not.toHaveBeenCalled();
+    self.promise.then(function() {
+      done();
+    });
 
     self.success(42);
-
-    setTimeout(function() {
-      expect(self.thenCallback).toHaveBeenCalledWith(42);
-      expect(self.errorCallback).not.toHaveBeenCalled();
-      done();
-    }, 0);
   });
 });
