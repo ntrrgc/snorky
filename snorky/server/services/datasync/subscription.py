bab05b4c5295fd977203a0218ef13c727a321610
Juan Luis Boya García <ntrrgc@gmail.com>
2014-07-26 12:11:04 +0200

    Adapted Subscription to Snorky

---
+++ snorky/server/services/datasync/subscription.py
@@ -0,0 +1,90 @@
+__all__ = ('Subscription', 'SubscriptionItem')
+
+
+class SubscriptionItem(object):
+    """A pair of dealer_name and query linked to a Subscription."""
+
+    __slots__ = ('dealer_name', 'query', 'subscription')
+
+    def __init__(self, dealer_name, query, subscription=None):
+        self.dealer_name = dealer_name
+        self.query = query
+        self.subscription = subscription
+
+
+class Subscription(object):
+    """
+    An object which represents a contract that may be signed by a Client to be
+    notified by one or more dealers, providing one or more queries for each
+    Dealer.
+
+    Each pair of Dealer and query is stored as a SubscriptionItem.
+
+    A Subscription distinguishes three states:
+
+    * Acquired: A Client has been associated with the Subscription (in other
+      words, it has signed the contract). In order for a client to acquire a
+      Subscription it needs to know its subscription_token.
+
+    * WaitingClient: No client has acquired the Subscription yet. The
+      Subscription object will store any deltas for the subscription items and
+      forward them to the Client when it acquires the Subscription.
+
+    * Stale: Time has passed since the Subscription was created but no Client
+      has acquired it. Subscriptions in this state will be removed by the
+      system in order to free memory.
+    """
+
+    __slots__ = ('token', 'client', '_awaited_client_buffer',
+            '_awaited_client_timeout', 'items')
+
+    def __init__(self, items):
+        self.token = None
+        self.client = None
+        self._awaited_client_buffer = []
+        self._awaited_client_timeout = None
+
+        self.items = items
+        for item in items:
+            item.subscription = self
+
+    def got_client(self, client):
+        """Links this subscription with a client. If there were deltas received
+        before, they will be sent now."""
+
+        if self.client is not None:
+            raise RuntimeError("Subscription already linked to a Client")
+
+        self.client = client
+        self.flush_awaited_client_buffer()
+
+    def lost_client(self):
+        """Unlinks this subscription from its client. New arrived deltas will
+        no further be delivered to it."""
+
+        self.client = None
+
+    def deliver_delta(self, delta):
+        """Sends a JSON encoded delta to the associated client.
+
+        If there is currently no associated client the delta will be stored to
+        be sent when a client arrives."""
+
+        if self.client is not None:
+            self.client.deliver_delta(delta)
+        else:
+            self._awaited_client_buffer.append(delta)
+
+    def flush_awaited_client_buffer(self):
+        """Sends deltas stored in the _awaited_client_buffer to the client and
+        clears the buffer."""
+
+        for delta in self._awaited_client_buffer:
+            self.deliver_delta(delta)
+
+        # Clear buffer
+        self._awaited_client_buffer[:] = []
+
+    def cancel_timeout(self, io_loop):
+        io_loop.remove_timeout(self._awaited_client_timeout)
+        self._awaited_client_timeout = None
---
+++ snorky/tests/test_subscription.py
@@ -0,0 +1,50 @@
+import unittest
+from mock import Mock
+from snorky.server.services.datasync.subscription import \
+        Subscription, SubscriptionItem
+
+
+class FakeClient(object):
+    def __init__(self):
+        self.deliver_delta = Mock()
+
+
+class FakeDelta(object):
+    # Subscription class does not use Delta's methods or properties, just
+    # passes the Delta object around.
+    pass
+
+
+class TestSubscription(unittest.TestCase):
+    def setUp(self):
+        self.subscription_item = SubscriptionItem('test_dealer', 'red')
+        self.client = FakeClient()
+        self.delta = FakeDelta()
+
+    def test_got_client(self):
+        subscription = Subscription([self.subscription_item])
+
+        # Client connects
+        subscription.got_client(self.client)
+        self.assertIs(self.client, subscription.client)
+        self.assertEqual(subscription._awaited_client_buffer, [])
+        self.assertFalse(self.client.deliver_delta.called)
+
+        # Client disconnects
+        subscription.lost_client()
+        self.assertIsNone(subscription.client)
+
+    def test_awaited_client_buffer(self):
+        subscription = Subscription([self.subscription_item])
+
+        # Send delta
+        subscription.deliver_delta(self.delta)
+
+        # Client connects
+        subscription.got_client(self.client)
+
+        # Delta should have arrived to client, untouched
+        self.client.deliver_delta.assert_called_once_with(self.delta)
+
+        # Awaited client buffer should be empty now
+        self.assertEqual(subscription._awaited_client_buffer, [])
