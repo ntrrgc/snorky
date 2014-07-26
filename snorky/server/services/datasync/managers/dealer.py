d9963bfbb79d5acec40217bb772ae49810a4e993
Juan Luis Boya García <ntrrgc@gmail.com>
2014-07-26 19:26:47 +0200

    Adapted DealerManager to Snorky

---
+++ snorky/server/services/datasync/managers/dealer.py
@@ -0,0 +1,66 @@
+from miau.common.types import MultiDict
+import itertools
+
+__all__ = ('UnknownDealer', 'UnknownModelClass', 'DealerManager')
+
+
+class UnknownDealer(Exception):
+    def __init__(self, dealer_name):
+        self.dealer_name = dealer_name
+
+    def __str__(self):
+        return "No dealer named '%s'." % self.dealer_name
+
+
+class UnknownModelClass(Exception):
+    def __init__(self, dealer_name):
+        self.dealer_name = dealer_name
+
+    def __str__(self):
+        return "No model class with name '%s' has been registered in the "\
+                "dealer manager" % self.dealer_name
+
+
+class DealerManager(object):
+    """Tracks dealers registered in the system."""
+
+    __slots__ = ('dealers_by_name', 'dealers_by_model_class')
+
+    def __init__(self):
+        self.dealers_by_name = {}
+        self.dealers_by_model_class = MultiDict()
+
+    def register_dealer(self, dealer):
+        """Registers a new dealer."""
+        name = dealer.name
+        model = dealer.model
+
+        if self.dealers_by_name.get(name):
+            raise RuntimeError(
+                    "Dealer with name '%s' already is registered." % name)
+
+        # Add to dealers_by_name
+        self.dealers_by_name[name] = dealer
+
+        # Add to dealers_by_model_class
+        self.dealers_by_model_class.add(model, dealer)
+
+    def get_dealer(self, name):
+        """Returns a dealer with the specified name"""
+        try:
+            return self.dealers_by_name[name]
+        except KeyError:
+            raise UnknownDealer(name)
+
+    def get_dealers_for_model_class(self, model):
+        """Returns an iterable of dealers with the specified model
+        """
+        try:
+            return self.dealers_by_model_class[model]
+        except KeyError:
+            raise UnknownModelClass(model)
+
+    def deliver_delta(self, delta, service):
+        # Deliver to associated dealers
+        for dealer in self.get_dealers_for_model_class(delta.model):
+            dealer.deliver_delta(delta, service)
---
+++ snorky/tests/test_dealer_manager.py
@@ -0,0 +1,75 @@
+import unittest
+from mock import Mock
+from snorky.server.services.datasync.managers.dealer import \
+        DealerManager, UnknownModelClass, UnknownDealer
+from snorky.server.services.datasync.delta import \
+        InsertionDelta, UpdateDelta, DeletionDelta
+
+
+class FakeService(object):
+    pass
+
+
+class FakeDealer(object):
+    def __init__(self, name, model):
+        self.name = name
+        self.model = model
+
+        self.deliver_delta = Mock()
+
+class TestDealerManager(unittest.TestCase):
+    def setUp(self):
+        self.service = FakeService()
+
+        self.dealer1 = FakeDealer('test_dealer1', 'test_model')
+        self.dealer2 = FakeDealer('test_dealer2', 'test_model')
+
+    def test_register_dealer(self):
+        dm = DealerManager()
+
+        # Register a dealer
+        dm.register_dealer(self.dealer1)
+        self.assertIs(dm.get_dealer('test_dealer1'), self.dealer1)
+        self.assertEqual(set(dm.get_dealers_for_model_class('test_model')),
+                {self.dealer1})
+
+        # Register a second dealer for the same model class
+        dm.register_dealer(self.dealer2)
+        self.assertIs(dm.get_dealer('test_dealer2'), self.dealer2)
+        self.assertEqual(set(dm.get_dealers_for_model_class('test_model')),
+                {self.dealer1, self.dealer2})
+
+    def test_get_unregistered_dealer(self):
+        dm = DealerManager()
+
+        with self.assertRaises(UnknownDealer):
+            dm.get_dealer('foo')
+
+    def test_deliver_delta(self):
+        dm = DealerManager()
+        dm.register_dealer(self.dealer1)
+
+        # Send a delta of the same model class of the registered dealer
+        delta = InsertionDelta('test_model', {'id':1})
+        dm.deliver_delta(delta, self.service)
+
+        self.dealer1.deliver_delta.assert_called_once_with(delta, self.service)
+
+    def test_deliver_delta_without_dealer(self):
+        dm = DealerManager()
+
+        # Send a delta of a model class without dealers
+        delta = InsertionDelta('test_model', {'id':1})
+
+        with self.assertRaises(UnknownModelClass):
+            dm.deliver_delta(delta, self.service)
+
+    def test_deliver_delta_other_model_class(self):
+        dm = DealerManager()
+        dm.register_dealer(self.dealer1)
+
+        # Send a delta of a model class without dealers
+        delta = InsertionDelta('unknown_model', {'id':1})
+
+        with self.assertRaises(UnknownModelClass):
+            dm.deliver_delta(delta, self.service)
