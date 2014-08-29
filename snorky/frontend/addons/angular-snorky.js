73c09e8b87b956d7554aa64dc0aa726a49b29020
Juan Luis Boya García <ntrrgc@gmail.com>
2014-08-29 23:10:11 +0200

    Added angular-snorky to the frontend directory

---
+++ snorky/frontend/addons/angular-snorky.js
@@ -0,0 +1,34 @@
+angular.module("Snorky", [])
+  .run(function($timeout) {
+    // Run $digest on events
+    var OldSignal = Snorky.Signal;
+    Snorky.Signal = function() {
+      OldSignal.apply(this, arguments);
+
+      var oldDispatch = this.dispatch;
+      this.dispatch = function() {
+        var self = this;
+        var args = arguments;
+        $timeout(function() {
+          oldDispatch.apply(self, args);
+        }, 0);
+      }
+    };
+    Snorky.Signal.prototype = new OldSignal();
+    Snorky.Signal.prototype.constructor = Snorky.Signal;
+  })
+  .run(function($q) {
+    // Use Angular promises
+    Snorky.Promise = function PromiseQAdapter(callback) {
+      var q = $q.defer();
+      var resolve = function(value) {
+        return q.resolve(value);
+      };
+      var reject = function(reason) {
+        return q.reject(reason);
+      };
+
+      callback(resolve, reject);
+      return q.promise;
+    };
+  })
