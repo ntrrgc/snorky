6ef6fd744ea44f69e46bf05d534784be63da75ca
Juan Luis Boya García <ntrrgc@gmail.com>
2014-09-04 16:01:26 +0200

    Client docstrings

--- snorky/client.py
+++ snorky/client.py
@@ -1,13 +1,22 @@
 from abc import abstractproperty, abstractmethod
 
 class Client(object):
+    """Represents a connection to the WebSocket server.
+
+    This is an abstract base class not associated to any protocol.
+    """
+
     def __init__(self):
         self.identity = None
 
     @abstractproperty
     def remote_address(self):
+        """An address to the remote client, usually an IP address."""
         pass
 
     @abstractmethod
     def send(self, msg):
+        """Send a message to the client. The message must be a JSON entity,
+        e.g. dictionary, string or number.
+        """
         pass
