b41360df3ea05fe7b7e2d812aad7c6b776bbc900
Juan Luis Boya García <ntrrgc@gmail.com>
2015-04-12 20:00:12 +0200

    Use relative paths

--- source/snorkyweb/settings.py
+++ source/snorkyweb/settings.py
@@ -45,7 +45,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = 'not_actually_used'
