c41aa821c7a1beb36c1e7d52b276400e8990bef6
Juan Luis Boya García <ntrrgc@gmail.com>
2015-04-12 18:52:58 +0200

    Use v prefix in tags

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ python2 -m unittest
 python3 -m unittest
 
 version=$(python -c 'import snorky; print(snorky.version)')
-git tag -a "$version"
+git tag -a "v${version}"
 python3 setup.py register -r pypi
 python3 setup.py sdist upload -r pypi
