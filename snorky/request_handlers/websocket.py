31f008b9116945fac630971c69d0fa2ea30462ca
Juan Luis Boya García <ntrrgc@gmail.com>
2015-04-14 03:07:03 +0200

    Add automatic WebSocket ping pong

--- snorky/request_handlers/websocket.py
+++ snorky/request_handlers/websocket.py
@@ -3,7 +3,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from tornado.websocket import WebSocketHandler
+from tornado.ioloop import IOLoop
 from snorky.client import Client
+from datetime import timedelta
 import json
 
 
@@ -31,7 +33,9 @@ class SnorkyWebSocketHandler(WebSocketHandler):
     """
     def __init__(self, *args, **kwargs):
         self.service_registry = kwargs.pop("service_registry")
+        self.ping_pong_interval = kwargs.pop('ping_pong_interval', 90) # seconds
         self.client = WebSocketClient(req_handler=self)
+
         super(SnorkyWebSocketHandler, self).__init__(*args, **kwargs)
 
     def check_origin(self, origin):
@@ -45,6 +49,23 @@ class SnorkyWebSocketHandler(WebSocketHandler):
         """Executed when the connection is started."""
         self.service_registry.client_connected(self.client)
 
+        if self.ping_pong_interval:
+            IOLoop.instance().add_timeout(
+                timedelta(seconds=self.ping_pong_interval),
+                self.its_ping_time)
+
+    def its_ping_time(self):
+        # Still alive?
+        if self.ws_connection:
+            # Send a ping frame
+            self.ping('')
+
+            # Repeat
+            if self.ping_pong_interval:
+                IOLoop.instance().add_timeout(
+                    timedelta(seconds=self.ping_pong_interval),
+                    self.its_ping_time)
+
     def on_message(self, message):
         """Called when a message is received."""
         self.service_registry.process_message_raw(self.client, message)
@@ -54,8 +75,10 @@ class SnorkyWebSocketHandler(WebSocketHandler):
         self.service_registry.client_disconnected(self.client)
 
     @classmethod
-    def get_route(cls, service_registry, path="/"):
+    def get_route(cls, service_registry, path="/", **kwargs):
         """Returns a route to this request handler, suitable for use in
         :py:class:`tornado.web.Application`.
         """
-        return (path, cls, {'service_registry': service_registry})
+        options = kwargs.copy()
+        options['service_registry'] = service_registry
+        return (path, cls, options)
