c6ed086d0090fbf9fea66ed3c684a70d2561b65b
Juan Luis Boya García <ntrrgc@gmail.com>
2016-02-17 22:13:46 +0100

    Build

--- source/generate.sh
+++ source/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d env ]; then
-  virtualenv -p $(which python3) env
+  virtualenv -p $(which python3.4) env
   source ./env/bin/activate
   pip install -r requirements.txt
 else
